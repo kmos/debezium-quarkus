cdcaba46ca098f6b4795796384739780e7115708
kmos <kmos@commonhaus.dev>
2025-12-23 16:54:10 +0100

    debezium/dbz#1484 introduce new data classes for batch processing
    Signed-off-by: kmos <kmos@commonhaus.dev>

---
+++ debezium-quarkus-parent/debezium-quarkus-engine-spi-parent/runtime/src/main/java/io/debezium/runtime/CapturingEvents.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.debezium.runtime;
+
+import java.util.List;
+
+public interface CapturingEvents<V> {
+
+    List<V> records();
+
+    /**
+     * @return logical destination for which the event are intended
+     */
+    String destination();
+
+    /**
+     *
+     * @return logical source for which the events are intended
+     */
+    String source();
+
+    /***
+     * @return engine for which the events are emitted
+     */
+    String engine();
+}
