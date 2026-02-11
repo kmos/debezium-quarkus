8a6f878a7d80fde2d2bb9590f66af412dfcb8b9c
kmos <kmos@commonhaus.dev>
2026-02-11 18:33:24 +0100

    debezium/dbz#34 create testing module
    Signed-off-by: kmos <kmos@commonhaus.dev>

--- debezium-quarkus-test/src/main/java/io/debezium/quarkus/test/assertions/DebeziumAssertions.java
+++ debezium-quarkus-test/src/main/java/io/debezium/quarkus/test/assertions/DebeziumAssertions.java
@@ -4,7 +4,7 @@
  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-package io.debezium.quarkus.hibernate.cache.deployment.assertions;
+package io.debezium.quarkus.test.assertions;
 
 import java.util.concurrent.TimeUnit;
 
