791a7d0c22a808381940e3ea7e32ffd0723caf6f
Chris Cranford <chris@hibernate.org>
2026-02-18 04:32:02 -0500

    debezium/dbz#1389 Fix alternative SQL script
    Signed-off-by: Chris Cranford <chris@hibernate.org>

--- debezium-quarkus-parent/debezium-quarkus-oracle-parent/integration-tests/src/main/resources/sql/alternative/02-setup-tables.sql
+++ debezium-quarkus-parent/debezium-quarkus-oracle-parent/integration-tests/src/main/resources/sql/alternative/02-setup-tables.sql
@@ -2,26 +2,11 @@
 CONNECT alternative/dbz@FREEPDB1;
 
 -- Setup USERS
-CREATE TABLE alternative.users (id NUMBER(9,0) PRIMARY KEY, name varchar2(255) NOT NULL);
-ALTER TABLE alternative.users ADD SUPPLEMENTAL LOG DATA (ALL) COLUMNS;
-GRANT SELECT ON alternative.users to c##dbzuser;
+CREATE TABLE alternative.orders (id NUMBER(9,0) PRIMARY KEY, name varchar2(255) NOT NULL, description varchar2(255) NOT NULL);
+ALTER TABLE alternative.orders ADD SUPPLEMENTAL LOG DATA (ALL) COLUMNS;
+GRANT SELECT ON alternative.orders to c##dbzuser;
 
-INSERT INTO alternative.users (id,name) values (1, 'alvar');
-INSERT INTO alternative.users (id,name) values (2, 'anisha');
-INSERT INTO alternative.users (id,name) values (3, 'chris');
-INSERT INTO alternative.users (id,name) values (4, 'indra');
-INSERT INTO alternative.users (id,name) values (5, 'jiri');
-INSERT INTO alternative.users (id,name) values (6, 'giovanni');
-INSERT INTO alternative.users (id,name) values (7, 'mario');
-INSERT INTO alternative.users (id,name) values (8, 'rené');
-INSERT INTO alternative.users (id,name) values (9, 'Vojtech');
-
--- Setup PRODUCTS
-CREATE TABLE alternative.products (id NUMBER(9,0) PRIMARY KEY, name varchar2(255) NOT NULL, description varchar2(255) NOT NULL);
-ALTER TABLE alternative.products ADD SUPPLEMENTAL LOG DATA (ALL) COLUMNS;
-GRANT SELECT ON alternative.products to c##dbzuser;
-
-INSERT INTO alternative.products (id,name,description) values (1,'t-shirt', 'red hat t-shirt');
-INSERT INTO alternative.products (id,name,description) values (2, 'sweatshirt', 'blue ibm sweatshirt');
+INSERT INTO alternative.orders (id,name,description) values (1, 'pizza', 'pizza with peperoni');
+INSERT INTO alternative.orders (id,name,description) values (2, 'kebab', 'kebab with mayonnaise');
 
 COMMIT;
